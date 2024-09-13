4501a9cf5acc51918d058dd00e0ea4da604a9513
anh188 <linh188abc@gmail.com>
2024-09-13 23:35:09 +0700

    rep

--- dantrang/assets/js/form-menu-footer.js
+++ dantrang/assets/js/form-menu-footer.js
@@ -1,15 +1,33 @@
 document.addEventListener ('DOMContentLoaded',function(){
+    
+    const headertimeHTML=`
+    <h6><i class="fa-solid fa-clock-rotate-left"></i> GIỜ MỞ CỬA:
+            Tất cả các ngày trong tuần
+            (Kể cả ngày Lễ, Tết)
+            Từ 8h00 đến 17h00</h6>
+            <div class="search-box">
+                <input type="text" placeholder="Tìm kiếm...">
+                <button class="btn-search"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
+    <img src="./assets/img/Flag_of_Vietnam.svg.webp" alt="">
+
+    `
+    document.querySelector('.header_time').insertAdjacentHTML('afterbegin', headertimeHTML);
+
+    
     const menuHTML=`
+        <i  onclick=showSidebar() class="icon-menu fa-solid fa-bars"></i>
+
     <div class="menu-icon">
         <img class="menu-icon-item" src="./assets/img/Group 2.1.png" alt="">
     </div>
-    <ul class="menu-list d-flex">
+    <ul class="menu-list">
         <li><a href="./index.html"><i class="fa-solid fa-house-chimney"></i>giới thiệu</a></li>
         <li><a href="">tin tức-sự kiện</a></li>
         <li><a href="">trưng bày</a></li>
         <li><a href="./education.html">giáo dục truyền thông</a></li>
         <li><a href="">sản phẩm đặc trưng</a></li>
-        <li><a href="">tài liệu</a>
+       <li><a href="">tài liệu<i class="fa-solid fa-chevron-down" style="margin-left: 4px;"></i></a>
             <ul class="document_menu">
                 <li><a href="./documentary.html">Phim tài liệu</a></li>
                 <li><a href="./gallery.html">thư viện ảnh</a></li>
@@ -24,10 +42,10 @@ document.addEventListener ('DOMContentLoaded',function(){
 
     const footerHTML=`
     <div class="infomation container">
-            <div class="logo">
+            <div class="infomation_item logo">
                 <img src="./assets/img/Group 2.1.png" alt="">
             </div>
-            <div class="info">
+            <div class="infomation_item info">
                 <h1 class="footer_h1">thông tin liên hệ</h1>
                 <div>
                     <p><i class="fa-solid fa-house-flag"></i>Số 1, phố Hỏa Lò, Hoàn Kiếm, Hà Nội</p>
@@ -37,7 +55,7 @@ document.addEventListener ('DOMContentLoaded',function(){
                     
                 </div>
             </div>
-            <div class="link">
+            <div class="infomation_item link">
                 <h1 class="footer_h1">thông tin liên hệ</h1>
                 <div class="link_item">
                     <div>
@@ -73,7 +91,7 @@ document.addEventListener ('DOMContentLoaded',function(){
     
                 </div>
             </div>
-            <div class="facebook">
+            <div class="infomation_item facebook">
                 <h1 class="footer_h1">thông tin liên hệ</h1>
                 <div class="facebook_item">
                     <div class="fb-page" data-href="https://www.facebook.com/pages/Khu-Di-T%C3%ADch-Nh%C3%A0-T%C3%B9-Ho%E1%BA%A3-L%C3%B2/290522211146747" data-tabs="timeline" data-width="" data-height="200px" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"></div>
---
+++ dantrang/assets/js/menurespon.js
@@ -0,0 +1,15 @@
+document.querySelector('.icon-menu').addEventListener('click', function() {
+    var menuList = document.querySelector('.menu-list');
+    // Kiểm tra giá trị hiện tại của display
+        menuList.style.display = 'block'; // Hiển thị menu nếu đang ẩn
+});
+function showSidebar() {
+    const sidebar = document.querySelector('.menu-list');
+    sidebar.style.setProperty('display', 'block', 'important');
+}
+$(document).ready(function(){
+    $('.fa-chevron-down').click(function(e){
+        e.preventDefault(); // Prevent the default action of the <a> tag
+        $(this).closest('a').next('.document_menu').slideToggle();
+    });
+});
